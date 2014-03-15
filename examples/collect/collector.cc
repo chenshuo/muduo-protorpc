29ac9ae98edb0aba5b12232a53657065b70e0fb4
Shuo Chen <chenshuo@chenshuo.com>
2014-03-14 22:22:07 -0700

    collector w/ service and inspector.

--- examples/collect/collector.cc
+++ examples/collect/collector.cc
@@ -1,4 +1,36 @@
+#include <examples/collect/collect.pb.h>
+
+#include <muduo/net/inspect/Inspector.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/EventLoopThread.h>
+#include <muduo/protorpc2/RpcServer.h>
+
+namespace collect
+{
+class CollectServiceImpl : public CollectService
+{
+ public:
+  virtual void getSnapshot(const ::collect::SnapshotRequestPtr& request,
+                           const ::collect::SystemInfo* responsePrototype,
+                           const ::muduo::net::RpcDoneCallback& done)
+  {
+    SystemInfo response;
+    // fill in
+    done(&response);
+  }
+};
+}
+
+using namespace muduo::net;
 
 int main(int argc, char* argv[])
 {
+  EventLoop loop;
+  Inspector ins(&loop, InetAddress(12345), "test");
+  ins.remove("pprof", "profile"); // remove 30s blocking
+  collect::CollectServiceImpl impl;
+  RpcServer server(&loop, InetAddress(54321));
+  server.registerService(&impl);
+  server.start();
+  loop.loop();
 }
